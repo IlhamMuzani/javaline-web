1ce6a919bd0bd96c51c436231991f8e4d1d14b87
IlhamMuzani <ilhammuzani21@gmail.com>
2024-05-02 08:33:03 +0700

    revisi

--- resources/views/admin/laporan_pengeluarankaskecilakun/index.blade.php
+++ resources/views/admin/laporan_pengeluarankaskecilakun/index.blade.php
@@ -51,7 +51,7 @@
                                 </select>
                             </div>
                             <div class="col-md-3 mb-3">
-                                <label for="status">Cari Kendaraan</label>
+                                <label for="status">Cari Akun</label>
                                 <select class="select2bs4 select2-hidden-accessible" name="barangakun_id"
                                     data-placeholder="Cari Kode.." style="width: 100%;" data-select2-id="23" tabindex="-1"
                                     aria-hidden="true" id="barangakun_id">
