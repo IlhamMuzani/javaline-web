4e4e5da051f6d698792d08b755912bd8bcc8fa7d
IlhamMuzani <ilhammuzani21@gmail.com>
2024-05-09 08:17:44 +0700

    akses perbarui

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -696,10 +696,12 @@ class UserController extends Controller
                     'laporan kasbon karyawan cari' => false,
                     'laporan kasbon karyawan cetak' => false,
 
+                    // posting continue
                     'posting memo perjalanan continue' => false,
                     'posting memo borong continue' => false,
                     'posting memo tambahan continue' => false,
 
+                    // unpost continue 
                     'unpost memo perjalanan continue' => false,
                     'unpost memo borong continue' => false,
                     'unpost memo tambahan continue' => false,
