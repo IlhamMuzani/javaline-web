dadc2202325f1d733a52047e772c89e25b00f79c
IlhamMuzani <ilhammuzani21@gmail.com>
2024-09-25 10:46:18 +0700

    perbaikan monitoring loadings

--- resources/views/admin/status_perjalanan/index.blade copy.php
+++ resources/views/admin/status_perjalanan/index.blade copy.php
@@ -307,79 +307,29 @@
             var progressText = document.getElementById('progressText');
             var progress = 0;
 
-            // Kirim form secara bersamaan (akan dihandle di atas saat proses selesai)
-            form.submit();
-
-            // Interval untuk meningkatkan progress hingga 50%
+            // Interval untuk meningkatkan progress hingga 90%
             var interval = setInterval(function() {
-                if (progress < 50) {
+                if (progress < 90) {
                     progress += 1;
                     progressBar.style.width = progress + '%';
                     progressText.innerText = progress + '%';
                 } else {
                     clearInterval(interval);
-                    // Berhenti sejenak sebelum melanjutkan
+                    // Simulasi proses reload
                     setTimeout(function() {
-                        // Mulai meningkatkan dari 50% ke 75%
-                        var nextInterval = setInterval(function() {
-                            if (progress < 75) {
-                                progress += 1;
-                                progressBar.style.width = progress + '%';
-                                progressText.innerText = progress + '%';
-                            } else {
-                                clearInterval(nextInterval);
-                                // Berhenti sejenak sebelum melanjutkan ke 90%
-                                setTimeout(function() {
-                                    // Mulai meningkatkan dari 75% ke 90%
-                                    var toNinetyInterval = setInterval(function() {
-                                            if (progress < 90) {
-                                                progress += 1;
-                                                progressBar.style.width = progress +
-                                                    '%';
-                                                progressText.innerText = progress + '%';
-                                            } else {
-                                                clearInterval(toNinetyInterval);
-                                                // Berhenti sejenak sebelum melanjutkan ke 100%
-                                                setTimeout(function() {
-                                                        // Tingkatkan dari 90% ke 100%
-                                                        var finalInterval =
-                                                            setInterval(function() {
-                                                                    if (progress <
-                                                                        99) {
-                                                                        progress +=
-                                                                            1;
-                                                                        progressBar
-                                                                            .style
-                                                                            .width =
-                                                                            progress +
-                                                                            '%';
-                                                                        progressText
-                                                                            .innerText =
-                                                                            progress +
-                                                                            '%';
-                                                                    } else {
-                                                                        clearInterval
-                                                                            (
-                                                                                finalInterval
-                                                                            );
-                                                                        // Form sudah disubmit sebelumnya
-                                                                    }
-                                                                },
-                                                                500
-                                                            ); // Ganti dengan durasi yang sesuai dengan waktu reload Anda
-                                                    },
-                                                    3000
-                                                ); // Ganti dengan durasi delay sebelum melanjutkan ke 100%
-                                            }
-                                        },
-                                        500
-                                    ); // Ganti dengan durasi yang sesuai untuk progress dari 75% ke 90%
-                                }, 1000); // Ganti dengan durasi delay setelah mencapai 75%
-                            }
-                        }, 500); // Ganti dengan durasi yang sesuai untuk progress dari 50% ke 75%
-                    }, 1000); // Ganti dengan durasi delay setelah mencapai 50%
+                        // Reload data selesai, set progress ke 100%
+                        progress = 100;
+                        progressBar.style.width = progress + '%';
+                        progressText.innerText = progress + '%';
+
+                        // Submit form setelah proses reload selesai
+                        form.submit();
+                    }, 20000); // Ganti dengan durasi yang sesuai dengan waktu reload Anda
                 }
-            }, 30); // Simulasi loading hingga 50%
+            }, 30); // Simulasi loading hingga 90% (30ms * 90 = 2700ms)
+
+            // Kirim form
+            form.submit(); // Ini mengirim form, tetapi akan dihandle di atas saat proses selesai
         }
     </script>
 
