0d986138832b60ae1fed8b2acf1c4c0df47df111
IlhamMuzani <ilhammuzani21@gmail.com>
2024-11-10 11:26:49 +0700

    hide user

--- app/Http/Controllers/Admin/UserpelangganController.php
+++ app/Http/Controllers/Admin/UserpelangganController.php
@@ -20,7 +20,7 @@ class UserpelangganController extends Controller
     {
         if (auth()->check() && auth()->user()->menu['user']) {
 
-            $this->storeAllPelanggan();
+            // $this->storeAllPelanggan();
 
             if ($request->has('keyword')) {
                 $keyword = $request->keyword;
