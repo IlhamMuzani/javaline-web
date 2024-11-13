1986e02b5d32816981f4507068fa36fbf3167039
IlhamMuzani <ilhammuzani21@gmail.com>
2024-11-13 07:25:11 +0700

    perbaikan alert

--- resources/views/admin/update_km/create.blade.php
+++ resources/views/admin/update_km/create.blade.php
@@ -30,7 +30,7 @@
                 <div class="alert alert-danger alert-dismissible">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                     <h5>
-                        <i class="icon fas fa-ban"></i> Error!
+                        <i class="icon fas fa-ban"></i> Gagal!
                     </h5>
                     @foreach (session('error') as $error)
                         - {{ $error }} <br>
--- resources/views/admin/upload/memo_ekspedisi/index.blade.php
+++ resources/views/admin/upload/memo_ekspedisi/index.blade.php
@@ -27,7 +27,7 @@
                 <div class="alert alert-danger alert-dismissible">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                     <h5>
-                        <i class="icon fas fa-ban"></i> Error!
+                        <i class="icon fas fa-ban"></i> Gagal!
                     </h5>
                     @foreach (session('error') as $error)
                         - {{ $error }} <br>
@@ -38,7 +38,7 @@
                 <div class="alert alert-danger alert-dismissible">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                     <h5>
-                        <i class="icon fas fa-ban"></i> Error!
+                        <i class="icon fas fa-ban"></i> Gagal!
                     </h5>
                     {{ session('erorrss') }}
                 </div>
@@ -48,7 +48,7 @@
                 <div class="alert alert-danger alert-dismissible">
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                     <h5>
-                        <i class="icon fas fa-ban"></i> Error!
+                        <i class="icon fas fa-ban"></i> Gagal!
                     </h5>
                     @if (session('error_pelanggans'))
                         @foreach (session('error_pelanggans') as $error)
