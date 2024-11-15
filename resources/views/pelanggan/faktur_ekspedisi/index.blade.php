bdc7d6a8ab2b43701fc1e2cca73b010f4c7a1b4e
IlhamMuzani <ilhammuzani21@gmail.com>
2024-11-15 10:14:02 +0700

    perbaikan view desain pelanggan

--- resources/views/pelanggan/faktur_ekspedisi/index.blade.php
+++ resources/views/pelanggan/faktur_ekspedisi/index.blade.php
@@ -66,14 +66,14 @@
     <div class="content-header" style="display: none;" id="mainContent">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-sm-6">
+                {{-- <div class="col-sm-6">
                     <h1 class="m-0">Faktur Ekspedisi</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item active">Faktur Ekspedisi</li>
                     </ol>
-                </div><!-- /.col -->
+                </div><!-- /.col --> --}}
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
--- resources/views/pelanggan/monitoring_suratjalan/index.blade.php
+++ resources/views/pelanggan/monitoring_suratjalan/index.blade.php
@@ -77,14 +77,14 @@
     <div class="content-header" style="display: none;" id="mainContent">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-sm-6">
+                {{-- <div class="col-sm-6">
                     <h1 class="m-0">Monitoring Surat Jalan</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item active">Monitoring Surat Jalan</li>
                     </ol>
-                </div><!-- /.col -->
+                </div><!-- /.col --> --}}
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
