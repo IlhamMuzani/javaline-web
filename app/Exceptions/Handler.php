dcfbeee38ca25f69dd4dd5041603ca4cfcb28f3e
IlhamMuzani <ilhammuzani21@gmail.com>
2024-11-18 11:36:42 +0700

    pesan errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 class Handler extends ExceptionHandler
 {
     /**
@@ -37,5 +38,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($this->isHttpException($exception)) {
+            return response()->view('errors.500', [], $exception->getStatusCode());
+        }
+
+        return parent::render($request, $exception);
     }
 }
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Oops! Something went wrong</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 50px;
+        }
+        h1 {
+            font-size: 50px;
+        }
+        p {
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Oops! 😢</h1>
+    <p>There seems to be an issue with the system. Please try again later.</p>
+    <p>If the issue persists, contact support.</p>
+</body>
+</html>
+javaline.id
