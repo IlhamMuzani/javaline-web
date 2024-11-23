fff586512cb5cf8df12a9df69f5a17d6fa8834a3
IlhamMuzani <ilhammuzani21@gmail.com>
2024-11-23 10:44:17 +0700

    perbaikan csf

--- app/Exports/RekapGajimExport.php
+++ app/Exports/RekapGajimExport.php
@@ -2,11 +2,13 @@
 
 namespace App\Exports;
 
-use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
+use App\Models\Detail_gajikaryawan;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithTitle;
 
-
-class RekapGajimExport implements FromView
+class RekapGajimExport implements FromCollection, WithHeadings, WithMapping, WithTitle
 {
     protected $perhitungan_gaji;
     protected $detail_gaji;
@@ -17,11 +19,143 @@ class RekapGajimExport implements FromView
         $this->detail_gaji = $detail_gaji;
     }
 
-    public function view(): View
+    public function collection()
     {
-        return view('admin.inquery_perhitungangaji.export_rekap', [
-            'perhitungan_gaji' => $this->perhitungan_gaji,
-            'detail_gaji' => $this->detail_gaji,
-        ]);
+        return $this->detail_gaji;
+    }
+
+    public function headings(): array
+    {
+        return [
+            '',
+            '',
+            '',
+            '',
+            '',
+            'P',
+            'yyyyMMdd',
+            'Debit Account No.',
+            'Total Records',
+            'Total Amount',
+            '',
+            '',
+            'To Acc No.',
+            'To Acc Name',
+            'To Acc Address 1',
+            'To Acc Address 2',
+            'To Acc Address 3',
+            'Transfer Currency',
+            'Transfer Amount',
+            'Transaction Remark',
+            'Customer Ref No.',
+            'FT Service',
+            'To Acc Bank Code',
+            'To Acc Bank Name',
+            'To Acc Bank Address 1',
+            'To Acc Bank Address 2',
+            'To Acc Bank Address 3',
+            'Bank City Name / Country Name',
+            'Beneficiary Notification Flag',
+            'Benef Notification E-mail',
+            'Organization Directory Name',
+            'Identical Status',
+            'Beneficiary Status',
+            'Beneficiary Citizenship',
+            'Purpose of Transaction',
+            'Remittance Code 1',
+            'Remittance Information 1',
+            'Remittance Code 2',
+            'Remittance Information 2',
+            'Remittance Code 3',
+            'Remittance Information 3',
+            'Remittance Code 4',
+            'Remittance Information 4',
+            'Instruction Code 1',
+            'Instruction Remark 1',
+            'Instruction Code 2',
+            'Instruction Remark 2',
+            'Instruction Code 3',
+            'Instruction Remark 3',
+            'Charge Instruction',
+            'SWIFT Method / Beneficiary Type',
+            'Extended Payment Detail',
+            'Special Rate Ref No',
+            'Underlying Document Code',
+            'Sender to Receiver Information Code 2',
+            'Sender to Receiver Information Line 2',
+            'Sender to Receiver Information Code 3',
+            'Sender to Receiver Information Line 3',
+            'Sender to Receiver Information Code 4',
+            'Sender to Receiver Information Code 5',
+            'Sender to Receiver Information Line 5',
+            'Sender to Receiver Information Code 6',
+            'Sender to Receiver Information Line 6'
+        ];
+    }
+
+    public function map($detail): array
+    {
+        return [
+            '',
+            '',
+            '',
+            '',
+            '',
+            'P',
+            str_replace('-', '', $this->perhitungan_gaji->tanggal_awal),
+            '1390088880046',
+            $this->perhitungan_gaji->detail_gajikaryawan->count(),
+            $this->perhitungan_gaji->grand_total,
+            '',
+            '',
+            $detail->karyawan->norek ?? '',
+            $detail->karyawan->nama_lengkap ?? '',
+            $detail->karyawan->alamat3 ?? '',
+            $detail->karyawan->alamat2 ?? '',
+            $detail->karyawan->alamat ?? '',
+            'IDR',
+            $detail->gaji_bersih,
+            'Trf InHouse',
+            '',
+            'IBU',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            'N',
+            $detail->karyawan->gmail ?? '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            'EPD' . ($this->collection()->search($detail) + 1),
+            '',
+            '//',
+            'info2',
+            '//',
+            'info3',
+            '//',
+            'info4',
+            '//',
+            'info5',
+            '//',
+            'info6',
+        ];
+    }
+
+    public function title(): string
+    {
+        return 'Rekap Gaji'; // Nama sheet
     }
 }
--- app/Http/Controllers/Admin/InqueryPerhitungangajiController.php
+++ app/Http/Controllers/Admin/InqueryPerhitungangajiController.php
@@ -602,6 +602,20 @@ class InqueryPerhitungangajiController extends Controller
     // }
 
 
+    // public function export_gm($id)
+    // {
+    //     $perhitungan_gaji = Perhitungan_gajikaryawan::find($id);
+
+    //     if (!$perhitungan_gaji) {
+    //         return redirect()->back()->withErrors(['error' => 'Data Perhitungan Gaji tidak ditemukan']);
+    //     }
+
+    //     $detail_gaji = Detail_gajikaryawan::where('perhitungan_gajikaryawan_id', $perhitungan_gaji->id)->get();
+
+    //     // Ekspor sebagai CSV
+    //     return Excel::download(new RekapGajimExport($perhitungan_gaji, $detail_gaji), 'rekap_gaji.csv', \Maatwebsite\Excel\Excel::CSV);
+    // }
+
     public function export_gm($id)
     {
         $perhitungan_gaji = Perhitungan_gajikaryawan::find($id);
