c128e1560abe823a79a3e98a178b144208b98b0e
IlhamMuzani <ilhammuzani21@gmail.com>
2024-12-03 17:32:52 +0700

    perbaikan export

--- app/Http/Controllers/Admin/InqueryPerhitungangajibulananController.php
+++ app/Http/Controllers/Admin/InqueryPerhitungangajibulananController.php
@@ -681,21 +681,6 @@ class InqueryPerhitungangajibulananController extends Controller
     }
 
 
-    // public function export_gm($id)
-    // {
-    //     $perhitungan_gaji = Perhitungan_gajikaryawan::find($id);
-
-    //     if (!$perhitungan_gaji) {
-    //         return redirect()->back()->withErrors(['error' => 'Data Perhitungan Gaji tidak ditemukan']);
-    //     }
-
-    //     $detail_gaji = Detail_gajikaryawan::where('perhitungan_gajikaryawan_id', $perhitungan_gaji->id)->get();
-
-    //     // Ekspor sebagai CSV
-    //     return Excel::download(new RekapGajimExport($perhitungan_gaji, $detail_gaji), 'rekap_gaji.csv', \Maatwebsite\Excel\Excel::CSV);
-    // }
-
-    
     public function export_gm($id)
     {
         $perhitungan_gaji = Perhitungan_gajikaryawan::find($id);
@@ -706,7 +691,22 @@ class InqueryPerhitungangajibulananController extends Controller
 
         $detail_gaji = Detail_gajikaryawan::where('perhitungan_gajikaryawan_id', $perhitungan_gaji->id)->get();
 
-        return Excel::download(new RekapGajimExport($perhitungan_gaji, $detail_gaji), 'rekap_gaji.xlsx');
+        // Ekspor sebagai CSV
+        return Excel::download(new RekapGajimExport($perhitungan_gaji, $detail_gaji), 'rekap_gaji.csv', \Maatwebsite\Excel\Excel::CSV);
     }
+
+    
+    // public function export_gm($id)
+    // {
+    //     $perhitungan_gaji = Perhitungan_gajikaryawan::find($id);
+
+    //     if (!$perhitungan_gaji) {
+    //         return redirect()->back()->withErrors(['error' => 'Data Perhitungan Gaji tidak ditemukan']);
+    //     }
+
+    //     $detail_gaji = Detail_gajikaryawan::where('perhitungan_gajikaryawan_id', $perhitungan_gaji->id)->get();
+
+    //     return Excel::download(new RekapGajimExport($perhitungan_gaji, $detail_gaji), 'rekap_gaji.xlsx');
+    // }
     
 }
